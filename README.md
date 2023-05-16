a90ec855a0f211eb2716dee2bebccd7043096fbc
arslankhan7286 <arslankhan7286@gmail.com>
2023-05-16 14:50:12 +0500

    added E-commerce-app-next-typescript

---
+++ README.md
@@ -0,0 +1,6 @@
+## E-Commerce App
+
+E-Commerce app created using Nextjs, Hooks and Typescript.
+
+Based on this project [react-Ecommerce-app]
+
---
+++ config.tsx
@@ -0,0 +1,11 @@
+export const DEFAULT_SEO = {
+  title: "A react e-commerce app for e-commerce",
+  description: 'A react e-commerce app using hooks, nextjs and typescript',
+  openGraph: {
+    type: 'website',
+    locale: 'en_IE',
+    url: '',
+    title: "A react e-commerce app for e-commerce",
+    description: 'A react e-commerce app using hooks, nextjs and typescript',
+  },
+};
